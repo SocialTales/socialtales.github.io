e94024c842ebb368aa1b5b85c5ac5a43a57eb2e5
Jean-Marc Lagace <jean-marc@m2i3.com>
2018-02-02 15:31:20 -0500

    initial release

---
+++ js/coming-soon.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "mp4/bg-doodle-10813.mp4", 
+    poster: "img/bg-doodle-10813-fallback.jpg"
+  }, {
+    posterType: 'jpg'
+  });
+
+})(jQuery); // End of use strict
